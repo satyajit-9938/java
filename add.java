f4f7993707343aaf0f419800582ea6d37497c9da
satyajit-9938 <satyajitmishra00007@gmail.com>
2025-04-23 11:23:02 +0530

    Calculation of two numbers

---
+++ add.java
@@ -0,0 +1,11 @@
+public class add {
+    public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("Total number is"+c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
