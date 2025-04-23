97569759317c8d37aa96fa691e0873ce72c448be
satyajit-9938 <satyajitmishra00007@gmail.com>
2025-04-23 11:51:45 +0530

    calculation of numbers

---
+++ cal.java
@@ -0,0 +1,20 @@
+ 
+    public class cal {
+        public static void main(String[] args) {
+            int a=10;
+            int b=20;
+            int c=a+b;
+            int d=a*b;
+            int e=b/a;
+            int f=a%b;
+            int g=b-a;
+            System.out.println("Total number is "+c);
+            System.out.println("d");
+            System.out.println("e");
+            System.out.println("f");
+            System.out.println("g");
+            // TODO Auto-generated method stub
+    
+        }
+    
+}
